f651f6323e777ca3d33c99d53e279e80ef5bb219
hritikkumar07 <hritiknaik16@gmail.com>
2026-06-14 12:49:59 +0530

    Refactor: remove database credentials and ignore excel files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect("mongodb+srv://snapbytesofficial_db_user:mrkanhu03042026@cluster0.3b6kbzs.mongodb.net/Expense").then(()=> console.log("DB CONNECTED"));
-
-
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+        console.error("MONGO_URI is not defined in environment variables!");
+        process.exit(1);
+    }
+    await mongoose.connect(mongoUri).then(()=> console.log("DB CONNECTED"));
 }
